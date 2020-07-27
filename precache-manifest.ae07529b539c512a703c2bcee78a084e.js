5a7204ea62e6a6ddf99e57bcf270ff7f56e243d3
stephn88 <snjenj01@mail.bbk.ac.uk>
2020-07-27 19:07:04 +0100

    Updates

---
+++ precache-manifest.ae07529b539c512a703c2bcee78a084e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52559d1cfa9f6db17ba17fbd5861292c",
+    "url": "/stephn88-module-ii/index.html"
+  },
+  {
+    "revision": "af02e91d626de9bdc539",
+    "url": "/stephn88-module-ii/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "c5b654446a56705d3aa9",
+    "url": "/stephn88-module-ii/static/css/main.c8a1cc54.chunk.css"
+  },
+  {
+    "revision": "af02e91d626de9bdc539",
+    "url": "/stephn88-module-ii/static/js/2.1155ed46.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/stephn88-module-ii/static/js/2.1155ed46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b654446a56705d3aa9",
+    "url": "/stephn88-module-ii/static/js/main.2d16d621.chunk.js"
+  },
+  {
+    "revision": "25b9c26760f75b67c610",
+    "url": "/stephn88-module-ii/static/js/runtime-main.18155e13.js"
+  },
+  {
+    "revision": "91d6350a9c2887e9847a443d083946b4",
+    "url": "/stephn88-module-ii/static/media/breathe.91d6350a.jpg"
+  },
+  {
+    "revision": "2024017418b1d496686c651150a44cb9",
+    "url": "/stephn88-module-ii/static/media/couple-fairy-lights.20240174.jpg"
+  },
+  {
+    "revision": "5173a8b947694a8bbf42245de269dbb7",
+    "url": "/stephn88-module-ii/static/media/couple-kiss.5173a8b9.jpg"
+  },
+  {
+    "revision": "3f3277b8987ccc9547371d4880b35791",
+    "url": "/stephn88-module-ii/static/media/flower-error.3f3277b8.png"
+  },
+  {
+    "revision": "74110d6c933aaee5e4998fc4d3cb32fa",
+    "url": "/stephn88-module-ii/static/media/groom-laugh.74110d6c.jpg"
+  },
+  {
+    "revision": "50318d25f8f7c2f82dcb0b632083dc0b",
+    "url": "/stephn88-module-ii/static/media/logo.50318d25.png"
+  }
+]);
